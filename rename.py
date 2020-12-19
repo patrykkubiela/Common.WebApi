513087fee1c85e9e133456585425da8c202f7d44
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2020-12-19 23:00:46 +0100

    create rename function and add some shell arguments for this script that will be used as files names to search and change

--- rename.py
+++ rename.py
@@ -1,17 +1,23 @@
 import os
 import sys
 
-# filenameParam = sys.argv[1]
-filenameParam = 'CommonWebApi'
+pathPart_to_change = sys.argv[1]
+newPhrase = sys.argv[2]
 
-print(filenameParam)
-rootPath = os.getcwd()
-oryginalProjectName = 'somenew'
+def rename_files(oldName, newName):
+    rootPath = os.getcwd()
 
-for root, dirs, files in os.walk(rootPath):
-    for filename in files:
-        if(oryginalProjectName in filename):
-            old_fullFileName = root + "/" + filename
-            newFileName = filename.replace(oryginalProjectName, "HealthShield.QRCode.Service")
-            new_fullFileName = root + "/" + newFileName
-            os.rename(old_fullFileName, new_fullFileName)
+    result = []
+    for root, dirs, files in os.walk(rootPath):
+        for filename in files:
+            if(oldName in filename):
+                old_fullFileName = root + "/" + filename
+                newFileName = filename.replace(oldName, newName)
+                new_fullFileName = root + "/" + newFileName
+                # os.rename(old_fullFileName, new_fullFileName)
+                result.append({old_fullFileName, new_fullFileName})
+
+    return result
+
+
+print(rename_files(pathPart_to_change, newPhrase))
